a5adff547e777220d40b2e08a8afcda6779f561a
Binn Loo <binn_loo@mymail.sutd.edu.sg>
2019-04-20 10:39:45 +0800

    fixed hidden tensor device

--- model.py
+++ model.py
@@ -44,8 +44,8 @@ class Decoder(nn.Module):
         
     def init_hidden(self,hc=None):
         if hc == None:
-            self.h0 = torch.zeros(self.num_layers,1,self.hidden_size)
-            self.c0 = torch.zeros(self.num_layers,1,self.hidden_size)
+            self.h0 = torch.zeros(self.num_layers,1,self.hidden_size).to(torch.device('cuda'))
+            self.c0 = torch.zeros(self.num_layers,1,self.hidden_size).to(torch.device('cuda'))
         else:
             self.h0,self.c0 = hc
         
